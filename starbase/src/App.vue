b2d93c413e88753c5457e33cd5a62151b0d9035f
pere.verges <pere.verges@jediupc.com>
2019-03-15 23:31:49 +0100

    Beggining of starbase, simple page to keep practicing vue

---
+++ starbase/src/App.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div id="app">
+    <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+      title: 'Generate Your Team'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people/1', {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(json => console.log(json))
+    }
+  }
+}
+</script>
