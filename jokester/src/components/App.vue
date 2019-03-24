79ca446c8ab473f25806025c0122588b079dd303
pere.verges <pere.verges@jediupc.com>
2019-03-24 18:46:56 +0100

    Getting all the data from the API

--- jokester/src/components/App.vue
+++ jokester/src/components/App.vue
@@ -1,11 +1,22 @@
 <template lang="html">
   <div id="app">
-    Got any jokes?
+
+    <h3>Got any jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes"> Add joke </button>
+    <div class="">
+          {{$store.state}}
+    </div>
+
   </div>
 </template>
 
+
 <script>
+import {mapActions} from 'vuex'
 export default {
+  methods: mapActions([
+    'initJokes'
+  ])
 }
 </script>
 
---
+++ jokester/src/store/actions.js
@@ -0,0 +1,8 @@
+import * as types from './mutation-types'
+
+export const initJokes = ({commit}) => {
+  fetch('https://official-joke-api.appspot.com/random_ten', {
+    method: 'GET'
+  }).then(response => response.json())
+    .then(json => commit(types.INIT_JOKES, json))
+}
