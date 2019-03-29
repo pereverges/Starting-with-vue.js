7ca0e280d21484b155262fe3ac4b5d2d86d54edd
pere.verges <pere.verges@jediupc.com>
2019-03-29 20:00:30 +0100

    Turn out finished and uplaoded with firebase

--- turnout/src/components/Dashboard.vue
+++ turnout/src/components/Dashboard.vue
@@ -2,17 +2,23 @@
   <div class="">
     <h3>Dashboard Component</h3>
     <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
-    {{$store.state}}
     <hr>
     <AddEvent />
     <hr>
+    <div class="row justify-content-md">
+      <EventItem
+        v-for="(event_item, index) in this.$store.state.events"
+        :event="event_item"
+        key="index" />
+    </div>
   </div>
 </template>
 
 <script>
 
-import {firebaseApp} from '../firebaseApp'
+import {firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
 
 export default{
   methods: {
@@ -22,7 +28,17 @@ export default{
     }
   },
   components: {
-    AddEvent
+    AddEvent,
+    EventItem
+  },
+  mounted(){
+    eventsRef.on('value', snap => {
+      let events = []
+      snap.forEach(event =>{
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)
+    })
   }
 }
 
